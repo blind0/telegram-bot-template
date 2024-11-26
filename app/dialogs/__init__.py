a8f7e27b6e6fb8ffb12c5dceb2bdb2ab9c166a45
love_cube <ahapka1@gmail.com>
2024-11-27 02:47:24 +0500

    update

--- app/dialogs/__init__.py
+++ app/dialogs/__init__.py
@@ -1,7 +1,14 @@
-from aiogram_dialog import DialogRegistry
+from aiogram import Router
+import os
+from importlib import import_module
 
 
-def register_dialogs(registry: DialogRegistry):
-    from . import sample_dialog
-
-    registry.register(sample_dialog.ui)
+def get_dialog_router() -> Router:
+    router = Router()
+    for module in os.listdir(os.path.dirname(__file__)):
+        if module == "__init__.py" or module[-3:] != ".py":
+            continue
+        router.include_router(
+            getattr(import_module(f".{module[:-3]}", __package__), "ui", None)
+        )
+    return router
